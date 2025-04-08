8f91f92a005563add458dd6d58b94e80e25ae39b
Shu-Xiao Liu <d3a2s1l@gmail.com>
2025-04-09 03:23:48 +0800

    update code

--- rename_file_TDMS_convert_Chatgpt.m
+++ rename_file_TDMS_convert_Chatgpt.m
@@ -1,27 +1,38 @@
 % parpool("Processes",4)
-
+tStart = tic;
 % 資料夾路徑
-%folder_path = 'E:\Peaksea\paper\broadbandSMSPD\BroadbandSMSPD_20240519\Laser\P1\20240520\4.5\Pulse\515\10000kHz\2000nW\0degrees\20240520_214942';
-folder_path = 'E:\SMSPD_NbTiN_1\Laser\1-1\20250108\12\Pulse\450\10000kHz\800nW\0degrees\20250108_011217';
+folder_path = 'D:\SNSPD_data\12K_515nm_30000nW\0degrees\20250106_213747';
+%folder_path = 'E:\SMSPD_NbTiN_1\Laser\1-1\20250108\12\Pulse\450\10000kHz\800nW\0degrees\20250108_011217';
 
 
 % 儲存檔案地址
 Save_Adress = folder_path;
 % Save_Adress = 'E:\SNSPD_data'; 
 
-% 實驗參數
-Exp_para = 'SMSPD_NbTiN_1_1-1_Pulse_450_800nW_0degrees_';
-pattern = 'Pulse_\d+_\d+nW_\d+degrees';  %% generate output dir name
-dir_name = regexp(Exp_para, pattern, 'match', 'once');
 % 資料夾中的所有 TDMS 檔案
 file_list = dir(fullfile(folder_path, '*.tdms'));
+if(isempty(file_list))
+    warning('Cannot find TDMS file!');
+end
+
+% 實驗參數
+% Exp_para = 'SMSPD_NbTiN_1_1-1_Pulse_450_800nW_0degrees_';
+Exp_para = 'SMSPD_NbTiN_1_1-1_Pulse_515_30000nW_0degrees_';
+pattern = 'Pulse_\d+_\d+nW_\d+degrees';  %% generate output dir name
+%dir_name = regexp(Exp_para, pattern, 'match', 'once');
+dir_name = regexp(file_list(1).name, pattern, 'match', 'once');
+
 
 
 % 初始化一個結構數組來存儲轉換後的數據
 converted_data = struct('voltage', [], 'signal', [], 'trigger', []);
 
+%number_TDMSfiles = length(file_list);
+number_TDMSfiles = 8;
 % 讀取和轉換所有 TDMS 檔案
-parfor i = 1:length(file_list)
+t1 = toc(tStart);
+tStart = tic;
+parfor i = 1:number_TDMSfiles
     % 原始檔案名稱（包含路徑）
     original_filename = fullfile(folder_path, file_list(i).name);
 
@@ -49,7 +60,8 @@ parfor i = 1:length(file_list)
         warning('檔案 %s 不存在，跳過該檔案。', filename_TDMS);
     end
 end
-
+t2 = toc(tStart);
+tStart = tic;
 
 % 將轉換後的數據保存到 txt 檔案
 if  ~exist(fullfile(Save_Adress,dir_name), 'dir')  % Check if the directory exists
@@ -57,16 +69,52 @@ if  ~exist(fullfile(Save_Adress,dir_name), 'dir')  % Check if the directory exis
     fprintf('Directory "%s" created.\n', fullfile(Save_Adress,dir_name));
 end
 
+%%
 disp(['Data is saved in ',fullfile(Save_Adress,dir_name)]);
-for i = 1:length(converted_data)
+
+
+for i = 1:number_TDMSfiles
     % 構造新檔案名稱
     filename_new = fullfile(Save_Adress,dir_name, [Exp_para, num2str(converted_data(i).voltage), '_mV.txt']);
 
     % 保存數據到 txt 文件
-    F = [converted_data(i).signal, converted_data(i).trigger];
+    F = [converted_data(i).signal,  converted_data(i).trigger];
     save(filename_new, 'F', '-ascii');  %% need to parallel
     disp([int2str(i),'/',int2str(length(converted_data)),' output file name: ', fullfile([Exp_para, num2str(converted_data(i).voltage), '_mV.txt'])])
 end
 
 
 disp('Done')
+disp(['Data is saved in ',fullfile(Save_Adress,dir_name)]);
+t3 = toc(tStart);
+
+%% test code
+
+% 
+% disp('new test')
+% filename_new = strings(1, 8); 
+% fid = double(8);
+% parfor i = 1:length(5)
+%     % Generate file name
+%     filename_new(i) = ['testttt',num2str(i),'.txt'];
+%     % Extract data: assumed to be N×2 matrix
+%     %F = [converted_data(i).signal, converted_data(i).trigger];
+%     % Open file for writing
+%     disp(filename_new(i));
+%     fid = fopen(filename_new(i), 'w');
+%     if fid == -1
+%         warning('Could not open file: %s', filename_new(i));
+%         continue;
+%     end
+% 
+%     % Write each row with scientific notation and 7 decimal places
+%     for j = 1:100 %size(converted_data(i).signal,1)
+%         %fprintf(fid, '%f\t%f\n', converted_data(i).signal(j), converted_data(i).trigger(j));
+%     end
+% 
+%     fclose(fid);
+% 
+%     % Optional: display progress
+%     fprintf('%d/%d saved: %s\n', i, length(converted_data), filename_new(i));
+% end
+% disp('done');
