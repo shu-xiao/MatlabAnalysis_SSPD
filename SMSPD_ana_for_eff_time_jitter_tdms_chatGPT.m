7c6f57b75e7c5f3801852cd96cb6cb12c5ba0d87
Shu-Xiao Liu <d3a2s1l@gmail.com>
2025-05-08 03:34:08 +0800

    fix bug of V cut and add tile layout

--- SMSPD_ana_for_eff_time_jitter_tdms_chatGPT.m
+++ SMSPD_ana_for_eff_time_jitter_tdms_chatGPT.m
@@ -1,10 +1,10 @@
 %% 資料夾路徑
 tic
 %import SMSPD_waveform_plot_ChatGPT.*
-folder_path = 'E:\SMSPD_NbTiN_1\Laser\1-1\20250107\12\Pulse\450\10000kHz\15000nW\0degrees\20250107_235851\Pulse_450_15000nW_0degrees';
+folder_path = 'E:\SNSPD\SNSPD_data\SMSPD_NbTiN_2025Apr\Laser\1-10\20250503\4.68\Pulse\800\80000kHz\300000nW\0degrees\20250503_013751\Pulse_800_300000nW_0degrees';
 
 % 實驗參數
-Exp_para = 'SMSPD_NbTiN_1_1-1_Pulse_450_30000nW_0degrees_';
+% Exp_para = 'SMSPD_NbTiN_1_1-1_Pulse_450_30000nW_0degrees_';
 
 % 電壓範圍'
 file_list = dir(fullfile(folder_path, '*_mV.txt'));
@@ -17,81 +17,100 @@ Va = sort(extract_mV_from_list(file_list));
 %Va = [30,25];
 
 %% 預分配效率數組
+% threshold
+STDEV_CUT = 0.02; % 標準差閾值
+V_CUT = 0.05; % voltage amplitude 閾值
+Nevent = 10000; % 1 到 10000
+DATA_LENGTH = 125.;
 
-Nevent = 1000; % 1 到 10000
-DATA_LENGTH = 1000.;
 eff = zeros(length(Va), 1);
-for k = 1:length(Va)
-%for k = 1:2
+
+% setting for tab with figures
+fig = uifigure('Name', 'Multi-Tab Plots', 'Position', [100 100 800 600]);
+tabgroup = uitabgroup(fig, 'Position', [50 50 700 500]);
+
+% for loop for different Ib, Vb files
+%for k = 1:length(Va)  
+for k = 1:2
     % d = load([Exp_para, num2str(Va(k)), '_mV.txt']);
     file_path = fullfile(folder_path,file_list(k).name);
     disp(['processing... ', num2str(k), '/',num2str(length(Va))])
+    
     d = load(file_path, '-ascii'); % 快速加載數據
-    
-    
     signal = d(:, 1);
     trigger = d(:, 2);
     clear d
 
     
     q = zeros(DATA_LENGTH, 1);
-    X = 0;
-    Z = NaN(Nevent, 1);
+    nPass = 0;
+    Vamplitude = NaN(Nevent, 1);
     jitter = NaN(Nevent, 1);
     sigma = zeros(Nevent, 1);
-    stdth = 0.02; % 標準差閾值
-    ds1th = 0.05; % ds1 閾值
+
 
     for i = 1:Nevent
-        r = (1:DATA_LENGTH) + DATA_LENGTH * (i);
-        s = signal(r);
+        r = (1:DATA_LENGTH) + DATA_LENGTH * (i); % index of i events
+        s = signal(r);  % signal in i event
 
         % 計算 sigma
-        sigma(i) = std(s(1:DATA_LENGTH));
+        sigma(i) = std(s(1:DATA_LENGTH));   % the range can be changed
 
-       if sigma(i) <= stdth
+       if sigma(i) <= STDEV_CUT
             q = s + q;
-            s1 = s(1:DATA_LENGTH);
-            sbg = s(1:DATA_LENGTH);
-            Z(i) = max(s1) - min(s1);
-            ds1 = diff(s1);
+            s1 = s(1:DATA_LENGTH);  % signal region
+            sbg = s(1:DATA_LENGTH); % control region
+            Vamplitude(i) = max(s1) - min(s1);  % signal amplitude
+            deltaSig = diff(s1);  % i+1 data point - i data point
             dtr = diff(trigger(r));
-            nds1 = find(ds1 == max(ds1), 1);
+            ndeltaSig = find(deltaSig == max(deltaSig), 1);
             ntr = find(dtr == max(dtr), 1);
-            jitter(i) = ntr(1) - nds1(1);
-            count = length(find(s1 > stdth)); 
+            jitter(i) = ntr(1) - ndeltaSig(1);
+            count = length(find(s1 > V_CUT)); 
             if count >= 2
                 count = 1;
+                nPass = nPass + 1;
             end
-            X = X + count;
+            % nPass = nPass + count;
         end
     end
 
     % 計算效率
-    Ef_event = length(find(sigma <= stdth)); 
-    eff(k) = X / Ef_event;
+    Ef_event = length(find(sigma <= STDEV_CUT)); 
+    eff(k) = nPass / Ef_event;
     % 顯示圖形
-    figure;
-    subplot(2, 2, 1);
-    plot(s1, 'g');
-    title('Signal');
+    %figure;
+    tab(k)=uitab(tabgroup,'Title', sprintf('Tab_%i', k));
+    t = tiledlayout(tab(k), 2, 2, 'Padding', 'compact', 'TileSpacing', 'compact');
+    %tab_axes = axes('parent',tab(k));
+    %hold(tab_axes,'on')
+    
+    %subplot(2, 2, 1);
+    tile1 = nexttile(t, 1);
+    plot(tile1,s1, 'g');
+    title(tile1,'Signal');
 
-    subplot(2, 2, 2);
-    histogram(sort(sigma));
-    title('Histogram of Sigma');
+    %subplot(2, 2, 2);
+    tile2 = nexttile(t, 2);
+    histogram(tile2,sort(sigma));
+    title(tile2,'Histogram of Sigma');
 
-    subplot(2, 2, 3);
-    plot(ds1, 'g');
-    title('ds1');
+    %subplot(2, 2, 3);
+    tile3 = nexttile(t, 3);
+    plot(tile3,deltaSig, 'g');
+    title(tile3,'deltaSig');
 
-    subplot(2, 2, 4);
-    plot(Z);
-    title('Z');
+    %subplot(2, 2, 4);
+    tile4 = nexttile(t, 4);
+    plot(tile4,Vamplitude);
+    title(tile4,'Vamplitude');
 end
+
 % 將結果保存到 txt 檔案
 F = [Va, eff];
-outputname = [basename, '_',num2str(ds1th),'_', num2str(stdth), '_mV_efficiency.txt'];
-save(outputname, 'F', '-ascii');
+outputname = [basename, '_',num2str(V_CUT),'_', num2str(STDEV_CUT), '_mV_efficiency.txt'];
+
+save(fullfile(folder_path, outputname), 'F', '-ascii');
 disp(['save data to ', outputname]);
 
 
