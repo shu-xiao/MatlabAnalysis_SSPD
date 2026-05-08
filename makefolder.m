781196b10b784c3c8b505e1b11dbc40b2108b827
Shu-Xiao Liu <d3a2s1l@gmail.com>
2026-05-08 23:29:59 +0800

    Untrack local-only files (data, figures, draft scripts) and update .gitignore

--- makefolder.m
+++
@@ -1,6 +0,0 @@
-cd('F:\20230826_SMSPD_Polarization\P1 good') 
-P = [4:7.5:184];
-for k = 1:length(P)
-Save_Adress = strcat('4.5K_1.2uW_',num2str(P(k)));
-mkdir(strcat('4.5K_1.2uW_',num2str(P(k))))
-end
